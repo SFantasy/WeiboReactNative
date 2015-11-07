9f4a64938187833673abdb4d46a0d66f5eb6e62f
Fantasy Shao <forever.fantasy27@gmail.com>
2015-11-07 12:20:57 +0800

    1. Add url module (confusing could not use the core node.js module) 2. Add Weibo OAuth

---
+++ app/Home/index.js
@@ -0,0 +1,20 @@
+// HomeView
+
+'use strict';
+
+var React = require('react-native');
+
+var {
+  View,
+  Text
+} = React;
+
+var HomeView = React.createClass({
+  render: function () {
+    return (
+      <Text>Home View</Text>
+    );
+  }
+});
+
+module.exports = HomeView;
--- index.ios.js
+++ index.ios.js
@@ -1,50 +1,85 @@
+/**
+ * 一简微博
+ *
+ * @author Fantasy0 <fantasyshao@icloud.com>
+ */
+
 'use strict';
 
+// Module dependencies
 var React = require('react-native');
+var url = require('url');
+
+var config = require('./app/config');
+var HomeView = require('./app/Home');
 
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  WebView
 } = React;
 
-var Yijian = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+var OAUTH_URL = [
+  'https://api.weibo.com/oauth2/authorize',
+  '?client_id=' + config.app_key,
+  '&response_type=code',
+  '&redirect_uri=' + config.redirect_uri
+].join('');
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+var Yijian = React.createClass({
+  getInitialState: function () {
+    return {
+      login: false
+    }
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  render: function() {
+    if (this.state.login) {
+      return (
+        <HomeView />
+      );
+    }
+
+    return (
+      <View style={{flex: 1}}>
+        <WebView
+          ref={'webview'}
+          url={OAUTH_URL}
+          automaticallyAdjustContentInsets={false}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          startInLoadingState={true}
+          scalesPageToFit={true} />
+      </View>);
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+  onNavigationStateChange: function (navState) {
+    var urlObj = url.parse(navState.url, true);
+    if (urlObj.pathname == url.parse(config.redirect_uri).pathname) {
+      // 获取code
+      var code = urlObj.query.code;
+      var auth_url = [config.auth_uri,
+        '?client_id=' + config.app_key,
+        '&client_secret=' + config.app_secret,
+        '&grant_type=authorization_code',
+        '&redirect_uri=' + config.redirect_uri,
+        '&code=' + code
+      ].join('');
+
+      fetch(auth_url, {
+        method: 'post'
+      }).then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            login: true,
+            token: responseData.access_token
+          });
+        })
+        .catch((err) => console.log(err))
+        .done();
+    }
+  }
 });
 
 AppRegistry.registerComponent('Yijian', () => Yijian);
