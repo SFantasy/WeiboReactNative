5eee8b853a2cf220053a64978aac2dae1fd99b6d
Fantasy Shao <forever.fantasy27@gmail.com>
2015-11-07 10:51:14 +0800

    Initial commit. :D

---
+++ app/config.example.js
@@ -0,0 +1,8 @@
+// Configuration
+
+module.exports = {
+  app_key: 'YOUR_APP_KEY',
+  app_secret: 'YOUR_APP_SECRET',
+  redirect_uri: 'https://api.weibo.com/oauth2/default.html',
+  auth_uri: 'https://api.weibo.com/oauth2/access_token'
+};
