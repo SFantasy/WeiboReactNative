37246a7c5ad7758a8f8db8f209ea1763d7b89db8
Fantasy Shao <forever.fantasy27@gmail.com>
2015-11-08 11:56:36 +0800

    TabBar of the app

--- app/Home/index.js
+++ app/Home/index.js
@@ -3,16 +3,72 @@
 'use strict';
 
 var React = require('react-native');
+var Icon = require("react-native-vector-icons/FontAwesome");
 
 var {
-  View,
-  Text
+  NavigatorIOS,
+  TabBarIOS
 } = React;
 
 var HomeView = React.createClass({
+  getInitialState: function () {
+    return {
+      selectedTab: 'home'
+    }
+  },
+
   render: function () {
     return (
-      <Text>Home View</Text>
+      <TabBarIOS tintColor={'#5ac8fb'}>
+        <Icon.TabBarItem
+          title="Home"
+          iconName="home"
+          selectedIconName="home"
+          selected={this.state.selectedTab === 'home'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'home'
+            });
+          }}>
+          <NavigatorIOS
+            initialRoute={{
+              title: 'Timeline',
+              component: require('../Timeline')
+            }}/>
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="Post"
+          iconName="plus"
+          selectedIconName="plus"
+          selected={this.state.selectedTab === 'post'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'post'
+            });
+          }}>
+          <NavigatorIOS
+            initialRoute={{
+              title: 'Post',
+              component: require('../Post')
+            }}/>
+        </Icon.TabBarItem>
+        <Icon.TabBarItem
+          title="Me"
+          iconName="user"
+          selectedIconName="user"
+          selected={this.state.selectedTab === 'user'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'user'
+            });
+          }}>
+          <NavigatorIOS
+            initialRoute={{
+              title: 'Me',
+              component: require('../Account')
+            }}/>
+        </Icon.TabBarItem>
+      </TabBarIOS>
     );
   }
 });
