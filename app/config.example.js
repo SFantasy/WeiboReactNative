a6f1f6b4cb29116b077f913f2de57d9cb22a33fe
Fantasy Shao <forever.fantasy27@gmail.com>
2015-11-08 11:56:55 +0800

    Update configuration

--- app/config.example.js
+++ app/config.example.js
@@ -4,5 +4,7 @@ module.exports = {
   app_key: 'YOUR_APP_KEY',
   app_secret: 'YOUR_APP_SECRET',
   redirect_uri: 'https://api.weibo.com/oauth2/default.html',
-  auth_uri: 'https://api.weibo.com/oauth2/access_token'
+  auth_uri: 'https://api.weibo.com/oauth2/access_token',
+  api_prefix: 'https://api.weibo.com/2/',
+  token_store_key: 'YIJIAN_ACCESS_TOKEN'
 };
