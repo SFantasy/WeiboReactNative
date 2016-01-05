9bfe43991a35c7f185f566bfcaf7d50124d4365c
Fantasy Shao <forever.fantasy27@gmail.com>
2016-01-05 13:38:35 +0800

    Make timeline works

--- app/Timeline/index.js
+++ app/Timeline/index.js
@@ -1,13 +1,48 @@
+// Module dependencies
 var React = require('react-native');
+var GiftedListView = require('react-native-gifted-listview');
+
+var WeiboCell = require('../components/WeiboCell');
+var config = require('../config');
+var styles = require('./style');
+var api = require('../api');
 
 var {
-  Text
+  View,
+  AsyncStorage
 } = React;
 
 module.exports = React.createClass({
-  render: function () {
+
+  componentDidMount () {
+
+  },
+
+  async onFetch (page = 1, callback, options) {
+    let accessToken = await AsyncStorage.getItem(config.token_store_key);
+    let uid = await AsyncStorage.getItem(config.uid_store_key);
+    let url = `${api.statuses.homeTimeline}?access_token=${accessToken}&uid=${uid}&page=${page}`;;
+
+    fetch(url)
+      .then(resData => resData.json())
+      .then(res => {
+        callback(res.statuses);
+      })
+      .catch(err => console.error(err))
+      .done();
+  },
+
+  render () {
     return (
-      <Text>Timeline</Text>
+      <View style={styles.container}>
+        <GiftedListView
+          rowView={rowData=> <WeiboCell data={rowData} />}
+          onFetch={this.onFetch}
+          firstLoader={true}
+          pagination={true}
+          refreshable={true}
+        />
+      </View>
     );
   }
 });
---
+++ app/Timeline/style.js
@@ -0,0 +1,10 @@
+var React = require('react-native');
+
+module.exports = React.StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingBottom: 42,
+    paddingTop: 20
+  }
+});
