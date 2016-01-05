8601443aa6d8a5801897dbad0f0094c37e3e173b
Fantasy Shao <forever.fantasy27@gmail.com>
2016-01-05 18:15:50 +0800

    Customize gifted-listview options

--- app/Timeline/index.js
+++ app/Timeline/index.js
@@ -1,15 +1,18 @@
 // Module dependencies
-var React = require('react-native');
-var GiftedListView = require('react-native-gifted-listview');
+const React = require('react-native');
+const GiftedListView = require('react-native-gifted-listview');
+const Spinner = require('react-native-spinkit');
 
-var WeiboCell = require('../components/WeiboCell');
-var config = require('../config');
-var styles = require('./style');
-var api = require('../api');
+const WeiboCell = require('../components/WeiboCell');
+const config = require('../config');
+const styles = require('./style');
+const api = require('../api');
 
-var {
+const {
   View,
-  AsyncStorage
+  Text,
+  AsyncStorage,
+  TouchableOpacity
 } = React;
 
 module.exports = React.createClass({
@@ -21,7 +24,7 @@ module.exports = React.createClass({
   async onFetch (page = 1, callback, options) {
     let accessToken = await AsyncStorage.getItem(config.token_store_key);
     let uid = await AsyncStorage.getItem(config.uid_store_key);
-    let url = `${api.statuses.homeTimeline}?access_token=${accessToken}&uid=${uid}&page=${page}`;;
+    let url = `${api.statuses.homeTimeline}?access_token=${accessToken}&uid=${uid}&page=${page}`;
 
     fetch(url)
       .then(resData => resData.json())
@@ -32,6 +35,62 @@ module.exports = React.createClass({
       .done();
   },
 
+  renderFetchingView () {
+    return (
+      <View style={styles.loadingView}>
+        <Spinner size={36} type="Bounce" color="#5ac8fb" />
+      </View>
+    );
+  },
+
+  renderAllLoadedView () {
+    return (
+      <View style={styles.loadingView}>
+        <Text>加载完成</Text>
+      </View>
+    );
+  },
+
+  renderWaitingView (paginateCallback) {
+    return (
+      <View style={styles.loadingView}>
+        <TouchableOpacity onPress={paginateCallback}>
+          <Text style={{color:'#777',fontSize:14}}>点击加载更多</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  },
+
+  renderRefreshableFetchingView () {
+    return (
+      <View style={styles.loadingView}>
+        <Spinner size={36} type="Wave" color="#5ac8fb" />
+      </View>
+    );
+  },
+
+  renderRefreshableWillRefreshView () {
+    return (
+      <View style={styles.loadingView}>
+        <Text style={styles.pullText}>⇡ 释放更新</Text>
+      </View>
+    );
+  },
+
+  renderRefreshableWaitingView () {
+    return (
+      <View style={styles.loadingView}>
+        <Text style={styles.pullText}>⇣ 下拉刷新</Text>
+      </View>
+    );
+  },
+
+  renderEmptyView () {
+    return (
+      <Text>No data yet.</Text>
+    );
+  },
+
   render () {
     return (
       <View style={styles.container}>
@@ -39,7 +98,18 @@ module.exports = React.createClass({
           rowView={rowData=> <WeiboCell data={rowData} />}
           onFetch={this.onFetch}
           firstLoader={true}
+
           pagination={true}
+          paginationFetchigView={this.renderFetchingView}
+          paginationAllLoadedView={this.renderAllLoadedView}
+          paginationWaitingView={this.renderWaitingView}
+
+          refreshableFetchingView={this.renderRefreshableFetchingView}
+          refreshableWillRefreshView={this.renderRefreshableWillRefreshView}
+          refreshableWaitingView={this.renderRefreshableWaitingView}
+
+          emptyView={this.renderEmptyView}
+
           refreshable={true}
         />
       </View>
--- app/Timeline/style.js
+++ app/Timeline/style.js
@@ -1,10 +1,20 @@
-var React = require('react-native');
+const React = require('react-native');
 
 module.exports = React.StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 10,
-    paddingBottom: 42,
+    paddingBottom: 48,
     paddingTop: 20
+  },
+  loadingView: {
+    flex: 1,
+    height: 50,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  pullText: {
+    fontSize: 14,
+    color: '#777'
   }
 });
