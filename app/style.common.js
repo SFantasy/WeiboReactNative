b7a66afff4a25ed7481f0f484ca23e1772fec9c3
Fantasy Shao <forever.fantasy27@gmail.com>
2016-01-23 11:11:21 +0800

    Remove some sensitive comments

--- app/style.common.js
+++ app/style.common.js
@@ -1,14 +1,3 @@
-/**
- * style.common.js
- *
- * Copyright 2015 dfyoo.com
- *
- * @description
- * @author shaochunhua@tuniu.com
- * @create 2016-01-10
- * @update 2016-01-10
- */
-
 module.exports = {
 
   BLACK: '#333',
