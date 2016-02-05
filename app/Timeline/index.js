93be2c1c925a7f2ba6de282428d49aa270fac9eb
Fantasy Shao <forever.fantasy27@gmail.com>
2016-02-05 15:25:47 +0800

    Use WeiboList in Timeline

--- app/Timeline/index.js
+++ app/Timeline/index.js
@@ -1,25 +1,15 @@
 // Module dependencies
 const React = require('react-native');
-const GiftedListView = require('react-native-gifted-listview');
-const Spinner = require('react-native-spinkit');
+const WeiboList = require('../components/WeiboList');
 
-const WeiboCell = require('../components/WeiboCell');
 const config = require('../config');
-const styles = require('./style');
 const api = require('../api');
 
 const {
-  View,
-  Text,
-  AsyncStorage,
-  TouchableOpacity
+  AsyncStorage
 } = React;
 
 module.exports = React.createClass({
-
-  componentDidMount () {
-
-  },
 
   async onFetch (page = 1, callback, options) {
     let accessToken = await AsyncStorage.getItem(config.token_store_key);
@@ -35,84 +25,7 @@ module.exports = React.createClass({
       .done();
   },
 
-  renderFetchingView () {
-    return (
-      <View style={styles.loadingView}>
-        <Spinner size={36} type="Bounce" color="#5ac8fb" />
-      </View>
-    );
-  },
-
-  renderAllLoadedView () {
-    return (
-      <View style={styles.loadingView}>
-        <Text>加载完成</Text>
-      </View>
-    );
-  },
-
-  renderWaitingView (paginateCallback) {
-    return (
-      <View style={styles.loadingView}>
-        <TouchableOpacity onPress={paginateCallback}>
-          <Text style={{color:'#777',fontSize:14}}>点击加载更多</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  },
-
-  renderRefreshableFetchingView () {
-    return (
-      <View style={styles.loadingView}>
-        <Spinner size={36} type="Wave" color="#5ac8fb" />
-      </View>
-    );
-  },
-
-  renderRefreshableWillRefreshView () {
-    return (
-      <View style={styles.loadingView}>
-        <Text style={styles.pullText}>⇡ 释放更新</Text>
-      </View>
-    );
-  },
-
-  renderRefreshableWaitingView () {
-    return (
-      <View style={styles.loadingView}>
-        <Text style={styles.pullText}>⇣ 下拉刷新</Text>
-      </View>
-    );
-  },
-
-  renderEmptyView () {
-    return (
-      <Text>No data yet.</Text>
-    );
-  },
-
   render () {
-    return (
-      <View style={styles.container}>
-        <GiftedListView
-          rowView={rowData=> <WeiboCell data={rowData} />}
-          onFetch={this.onFetch}
-          firstLoader={true}
-
-          pagination={true}
-          paginationFetchigView={this.renderFetchingView}
-          paginationAllLoadedView={this.renderAllLoadedView}
-          paginationWaitingView={this.renderWaitingView}
-
-          refreshableFetchingView={this.renderRefreshableFetchingView}
-          refreshableWillRefreshView={this.renderRefreshableWillRefreshView}
-          refreshableWaitingView={this.renderRefreshableWaitingView}
-
-          emptyView={this.renderEmptyView}
-
-          refreshable={true}
-        />
-      </View>
-    );
+    return <WeiboList onFetch={this.onFetch} />;
   }
 });
